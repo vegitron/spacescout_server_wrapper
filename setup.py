afed939f2fabef5a1a65628ff0a96c64bbde294b
Craig M. Stimmel <cstimmel@uw.edu>
2016-02-05 15:00:46 -0800

    Checkout the "qa" branch (for test host deploy)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ submodule_path = os.path.join(path, "spotseeker_server")
 
 subprocess.call(["git", "submodule", "init"], cwd=path)
 subprocess.call(["git", "submodule", "update"], cwd=path)
-subprocess.call(["git", "submodule", "foreach", "git", "pull", "origin", "master"], cwd=path)
+subprocess.call(["git", "submodule", "foreach", "git", "pull", "origin", "qa"], cwd=path)
 
 
 setup(name='SpaceScout-Server-Wrapper',
